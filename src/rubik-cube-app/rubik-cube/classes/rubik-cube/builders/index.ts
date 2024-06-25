fa85fc9080b045dfc832c62fa106274c026ea6de
user1808 <tomasz.kozak04@gmail.com>
2024-06-26 00:40:47 +0200

    feat: update and create a new one index files

---
+++ src/rubik-cube-app/rubik-cube/classes/rubik-cube/builders/index.ts
@@ -0,0 +1,5 @@
+import { RubikCubeBuilder } from './rubik-cube-builder';
+import { RubikCubePieceBuilder } from './rubik-cube-piece-builder';
+import { RubikCubeShellBuilder } from './rubik-cube-shell-builder';
+
+export { RubikCubeBuilder, RubikCubePieceBuilder, RubikCubeShellBuilder };
--- src/rubik-cube-app/rubik-cube/interfaces/builders/index.ts
+++ src/rubik-cube-app/rubik-cube/interfaces/builders/index.ts
@@ -1,4 +1,5 @@
 import type { IRubikCubeBuilder } from './rubik-cube-builder';
 import type { IRubikCubePieceBuilder } from './rubik-cube-piece-builder';
+import type { IRubikCubeShellBuilder } from './rubik-cube-shell-builder';
 
-export type { IRubikCubeBuilder, IRubikCubePieceBuilder };
+export type { IRubikCubeBuilder, IRubikCubePieceBuilder, IRubikCubeShellBuilder };
