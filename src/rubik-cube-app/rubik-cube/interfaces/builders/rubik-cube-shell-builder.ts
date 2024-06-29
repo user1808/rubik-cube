a59aa9edcacb89a859fdd5c19e8e27de9d006ddf
user1808 <tomasz.kozak04@gmail.com>
2024-06-29 14:07:53 +0200

    feat: create build method

--- src/rubik-cube-app/rubik-cube/interfaces/builders/rubik-cube-shell-builder.ts
+++ src/rubik-cube-app/rubik-cube/interfaces/builders/rubik-cube-shell-builder.ts
@@ -1,1 +1,9 @@
-export interface IRubikCubeShellBuilder {}
+import type { IRubikCubeShell } from '../structure';
+
+export interface IRubikCubeShellBuilder<
+  TCubeRotationGroups extends string,
+  TCubeRotationTypes extends string,
+  TCubeShellPieces extends string,
+> {
+  buildShell(): IRubikCubeShell<TCubeRotationGroups, TCubeRotationTypes, TCubeShellPieces>;
+}
