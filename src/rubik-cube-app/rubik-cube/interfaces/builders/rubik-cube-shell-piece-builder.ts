f6cfef822f3806f032f145ea281c0cefa7cb5170
user1808 <tomasz.kozak04@gmail.com>
2024-06-29 20:19:45 +0200

    feat: create interfaces methods

--- src/rubik-cube-app/rubik-cube/interfaces/builders/rubik-cube-shell-piece-builder.ts
+++ src/rubik-cube-app/rubik-cube/interfaces/builders/rubik-cube-shell-piece-builder.ts
@@ -1,1 +1,9 @@
-export interface IRubikCubeShellPieceBuilder {}
+import type { IRubikCubeShellPiece } from '../structure';
+
+export interface IRubikCubeShellPieceBuilder<
+  TCubeRotationGroups extends string,
+  TCubeRotationTypes extends string,
+  TCubeShellPiece extends string,
+> {
+  buildShellPiece(): IRubikCubeShellPiece<TCubeRotationGroups, TCubeRotationTypes, TCubeShellPiece>;
+}
--- src/rubik-cube-app/rubik-cube/interfaces/builders/rubik-cube-shell-pieces-builder.ts
+++ src/rubik-cube-app/rubik-cube/interfaces/builders/rubik-cube-shell-pieces-builder.ts
@@ -1,1 +1,13 @@
-export interface IRubikCubeShellPiecesBuilder {}
+import type { IRubikCubeShell } from '../structure';
+
+export interface IRubikCubeShellPiecesBuilder<
+  TCubeRotationGroups extends string,
+  TCubeRotationTypes extends string,
+  TCubeShellPieces extends string,
+> {
+  buildShellPieces(): IRubikCubeShell<
+    TCubeRotationGroups,
+    TCubeRotationTypes,
+    TCubeShellPieces
+  >['pieces'];
+}
