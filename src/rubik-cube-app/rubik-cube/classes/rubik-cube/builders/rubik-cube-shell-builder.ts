0cee8da9b423d492c2815dcbb8e1982c59b08924
user1808 <tomasz.kozak04@gmail.com>
2024-06-29 18:02:49 +0200

    feat: amend shell builder

--- src/rubik-cube-app/rubik-cube/classes/rubik-cube/builders/rubik-cube-shell-builder.ts
+++ src/rubik-cube-app/rubik-cube/classes/rubik-cube/builders/rubik-cube-shell-builder.ts
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import type { IRubikCubeShellBuilder } from '@/rubik-cube-app/rubik-cube/interfaces/builders';
-import type { IRubikCubeShellData } from '@/rubik-cube-app/rubik-cube/interfaces/data';
 import type {
   IRubikCubeShell,
   IRubikCubeShellPiece,
@@ -8,47 +7,38 @@ import type {
 import type { GLTF } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { RubikCubeShellPiece } from '../structure/shell/rubik-cube-shell-piece';
 import { RubikCubeShell } from '../structure/shell/rubik-cube-shell';
+import type { TShellPieceData } from '@/rubik-cube-app/rubik-cube/types/rubik-cube';
 
 export class RubikCubeShellBuilder<
   TCubeRotationGroups extends string,
   TCubeRotationTypes extends string,
-  TCubeShellFilename extends string,
   TCubeShellPieces extends string,
-> implements
-    IRubikCubeShellBuilder<
-      TCubeRotationGroups,
-      TCubeRotationTypes,
-      TCubeShellFilename,
-      TCubeShellPieces
-    >
+> implements IRubikCubeShellBuilder<TCubeRotationGroups, TCubeRotationTypes, TCubeShellPieces>
 {
   constructor(
+    private readonly loadedGLTFCubeShell: GLTF,
+    private readonly shellPiecesData: Record<
+      TCubeShellPieces,
+      TShellPieceData<TCubeRotationGroups, TCubeRotationTypes>
+    >,
     private readonly cubeShellMaterial: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({
       transparent: true,
       opacity: 0,
     }),
   ) {}
 
-  public buildShell(
-    loadedGLTFCubeShell: GLTF,
-    cubeShellData: IRubikCubeShellData<
-      TCubeRotationGroups,
-      TCubeRotationTypes,
-      TCubeShellFilename,
-      TCubeShellPieces
-    >,
-  ): IRubikCubeShell<TCubeRotationGroups, TCubeRotationTypes, TCubeShellPieces> {
+  public buildShell(): IRubikCubeShell<TCubeRotationGroups, TCubeRotationTypes, TCubeShellPieces> {
     const shellPieces: {
       [TCubeShellPiece in TCubeShellPieces]: IRubikCubeShellPiece<
         TCubeRotationGroups,
         TCubeRotationTypes,
         TCubeShellPiece
       >;
-    } = loadedGLTFCubeShell.scene.children.reduce(
+    } = this.loadedGLTFCubeShell.scene.children.reduce(
       (acc, cur) => {
         const piece = cur as THREE.Mesh;
         const name = cur.name as TCubeShellPieces;
-        const data = cubeShellData.piecesData[name];
+        const data = this.shellPiecesData[name];
 
         return {
           ...acc,
