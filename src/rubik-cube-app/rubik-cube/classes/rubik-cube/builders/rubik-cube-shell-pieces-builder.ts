a96f6afde5c7edf3ab7712ec11284b2ea1a41f66
user1808 <tomasz.kozak04@gmail.com>
2024-06-29 21:57:52 +0200

    feat: add type guards

---
+++ src/rubik-cube-app/rubik-cube/classes/rubik-cube/builders/rubik-cube-shell-pieces-builder.ts
@@ -0,0 +1,53 @@
+import * as THREE from 'three';
+import type { IRubikCubeShellPiecesBuilder } from '@/rubik-cube-app/rubik-cube/interfaces/builders/rubik-cube-shell-pieces-builder';
+import type { IRubikCubeShell } from '@/rubik-cube-app/rubik-cube/interfaces/structure';
+import type { GLTF } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { RubikCubeShellPiece } from '../structure/shell/rubik-cube-shell-piece';
+import type { TShellPieceData } from '@/rubik-cube-app/rubik-cube/types/rubik-cube';
+import { TypeGuards } from '@/utils/type-guards';
+
+export class RubikCubeShellPiecesBuilder<
+  TCubeRotationGroups extends string,
+  TCubeRotationTypes extends string,
+  TCubeShellPieces extends string,
+> implements
+    IRubikCubeShellPiecesBuilder<TCubeRotationGroups, TCubeRotationTypes, TCubeShellPieces>
+{
+  constructor(
+    private readonly loadedGLTFCubeShell: GLTF,
+    private readonly shellPiecesData: Record<
+      TCubeShellPieces,
+      TShellPieceData<TCubeRotationGroups, TCubeRotationTypes>
+    >,
+    private readonly shellMaterial: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({
+      transparent: true,
+      opacity: 0,
+    }),
+  ) {}
+
+  public buildShellPieces(): IRubikCubeShell<
+    TCubeRotationGroups,
+    TCubeRotationTypes,
+    TCubeShellPieces
+  >['pieces'] {
+    return this.loadedGLTFCubeShell.scene.children.reduce(
+      (shellPieces, currentPiece) => {
+        if (!TypeGuards.isT(currentPiece, THREE.Mesh)) return shellPieces;
+
+        const name = currentPiece.name;
+        if (!TypeGuards.isObjectKey(name, this.shellPiecesData)) return shellPieces;
+
+        return {
+          ...shellPieces,
+          [name]: new RubikCubeShellPiece(
+            name,
+            this.shellPiecesData[name],
+            currentPiece.geometry,
+            this.shellMaterial,
+          ),
+        };
+      },
+      {} as IRubikCubeShell<TCubeRotationGroups, TCubeRotationTypes, TCubeShellPieces>['pieces'],
+    );
+  }
+}
