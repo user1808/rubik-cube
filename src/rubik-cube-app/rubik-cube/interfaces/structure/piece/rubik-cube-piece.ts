93181b10580e35a142e436df6210c44a8ef07ee8
user1808 <tomasz.kozak04@gmail.com>
2024-10-20 21:18:02 +0200

    feat: add visible faces field to interface

--- src/rubik-cube-app/rubik-cube/interfaces/structure/piece/rubik-cube-piece.ts
+++ src/rubik-cube-app/rubik-cube/interfaces/structure/piece/rubik-cube-piece.ts
@@ -1,10 +1,12 @@
 import * as THREE from 'three';
 import type { IRubikCubePieceFace } from './rubik-cube-piece-face';
 import type { TPieceId } from '@/rubik-cube-app/rubik-cube/types/rubik-cube';
+import type { RubikCubePieceVisibleFace } from '@/rubik-cube-app/rubik-cube/classes/rubik-cube/structure/piece/rubik-cube-piece-visible-face';
 
 export interface IRubikCubePiece extends THREE.Group {
   readonly pieceId: TPieceId;
   readonly pieceAllFaces: Array<IRubikCubePieceFace>;
+  readonly pieceVisibleFaces: Array<RubikCubePieceVisibleFace>;
 
   dispose(): void;
 }
