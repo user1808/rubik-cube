36c20cbdf8a72365065637787f1a4fe13de6867d
user1808 <tomasz.kozak04@gmail.com>
2024-10-20 13:42:41 +0200

    feat: export new interface via index file

--- src/rubik-cube-app/rubik-cube/interfaces/builders/index.ts
+++ src/rubik-cube-app/rubik-cube/interfaces/builders/index.ts
@@ -1,4 +1,5 @@
 import type { IRubikCubeBuilder } from './rubik-cube-builder';
+import type { IRubikCubeFacesBuilder } from './rubik-cube-faces-builder';
 import type { IRubikCubePieceBuilder } from './rubik-cube-piece-builder';
 import type { IRubikCubePiecesBuilder } from './rubik-cube-pieces-builder';
 import type { IRubikCubeRotationGroupsBuilder } from './rubik-cube-rotation-groups-builder';
@@ -7,6 +8,7 @@ import type { IRubikCubeShellPiecesBuilder } from './rubik-cube-shell-pieces-bui
 
 export type {
   IRubikCubeBuilder,
+  IRubikCubeFacesBuilder,
   IRubikCubePieceBuilder,
   IRubikCubePiecesBuilder,
   IRubikCubeRotationGroupsBuilder,
