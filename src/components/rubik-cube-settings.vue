120517dadeef37f67e0f56c23526d2ae69960b63
user1808 <tomasz.kozak04@gmail.com>
2024-11-27 23:01:19 +0100

    feat: made it resizable

--- src/components/rubik-cube-settings.vue
+++ src/components/rubik-cube-settings.vue
@@ -1,13 +1,21 @@
 <template>
   <UseDraggable
-    class="fixed z-50 select-none bg-white"
+    class="fixed z-50 select-none resize overflow-auto rounded-md bg-white p-2"
     v-slot="{ x, y }"
     :initial-value="{ x: 10, y: 10 }"
+    :prevent-default="true"
+    :handle="handle"
   >
-    Drag me! I am at {{ x }}, {{ y }}
+    <div ref="handle" class="flex flex-col">
+      <span>Settings {{ x }} {{ y }}</span>
+    </div>
+    <div class="h-5" />
   </UseDraggable>
 </template>
 
 <script setup lang="ts">
 import { UseDraggable } from '@vueuse/components';
+import { ref } from 'vue';
+
+const handle = ref<HTMLElement | null>(null);
 </script>
