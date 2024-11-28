8a7c9c5d351604a6136238251b361ba6bb77136e
user1808 <tomasz.kozak04@gmail.com>
2024-11-28 21:40:50 +0100

    feat: fun with vue use in progress

--- src/components/rubik-cube-settings.vue
+++ src/components/rubik-cube-settings.vue
@@ -1,28 +1,26 @@
 <template>
   <UseDraggable
-    class="fixed z-50 select-none resize overflow-auto rounded-md border border-gray-800 bg-black/60 p-2"
+    class="fixed z-50 size-1/2 min-w-min max-w-full select-none resize overflow-auto rounded-md border border-gray-800 bg-black/60 p-2"
     v-slot="{ x, y }"
-    :initial-value="{ x: 10, y: 10 }"
+    :initial-value="{ x: windowWidth / 4, y: windowHeight / 4 }"
     :prevent-default="true"
     :handle="handle"
   >
-    <UseElementSize v-slot="{ width, height }" class="size-full min-w-min max-w-full">
-      <div ref="handle" class="flex flex-col text-white">
-        <span>Settings {{ x }} {{ y }} {{ width }} {{ height }}</span>
-        <div class="flex gap-4">
-          <div v-for="i in 10" :key="i" class="size-5 bg-green-500">
-            {{ i }}
-          </div>
+    <div ref="handle" class="flex flex-col text-white">
+      <span>Settings {{ x }} {{ y }}</span>
+      <div class="flex gap-4">
+        <div v-for="i in 10" :key="i" class="size-5 bg-green-500">
+          {{ i }}
         </div>
       </div>
-      <div class="h-5" />
-    </UseElementSize>
+    </div>
+    <div class="h-5" />
   </UseDraggable>
 </template>
 
 <script setup lang="ts">
 import { useWindowSize } from '@vueuse/core';
-import { UseDraggable, UseElementSize } from '@vueuse/components';
+import { UseDraggable } from '@vueuse/components';
 import { ref } from 'vue';
 
 const { width: windowWidth, height: windowHeight } = useWindowSize();
