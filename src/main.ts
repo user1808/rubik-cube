dc614cd441400082215ba23633e99e402090c6c4
user1808 <tomasz.kozak04@gmail.com>
2025-01-14 19:38:21 +0100

    feat: amend main style and add meta tags

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createHead } from '@unhead/vue';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import PrimeVue from 'primevue/config';
 import App from './App.vue';
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
-createApp(App).use(pinia).use(PrimeVue, { theme: 'none' }).mount('#app');
+const head = createHead();
+createApp(App).use(pinia).use(PrimeVue, { theme: 'none' }).use(head).mount('#app');
--- src/views/rubik-cube-app-view.vue
+++ src/views/rubik-cube-app-view.vue
@@ -8,4 +8,26 @@
 <script setup lang="ts">
 import RubikCubeCanvas from '@/components/rubik-cube-canvas.vue';
 import RubikCubeSettings from '@/components/rubik-cube-settings.vue';
+import { useHead } from '@unhead/vue';
+
+useHead({
+  title: 'Cube App',
+  link: [
+    {
+      rel: 'author',
+      href: 'https://github.com/user1808',
+    },
+  ],
+  meta: [
+    {
+      name: 'theme-color',
+      content: '#000000',
+    },
+    {
+      name: 'description',
+      content:
+        'Cube App that allows you to play with a variety of cubes, including such as Ordinary Cube, Pyraminx and Megaminx.',
+    },
+  ],
+});
 </script>
