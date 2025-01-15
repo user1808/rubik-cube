5a0f137daff2e0b0336a61dc193a5aeb6783c182
user1808 <tomasz.kozak04@gmail.com>
2025-01-15 21:47:29 +0100

    feat: add fullscreen experience

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import { createHead } from '@unhead/vue';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import PrimeVue from 'primevue/config';
 import App from './App.vue';
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
-const head = createHead();
-createApp(App).use(pinia).use(PrimeVue, { theme: 'none' }).use(head).mount('#app');
+createApp(App).use(pinia).use(PrimeVue, { theme: 'none' }).mount('#app');
