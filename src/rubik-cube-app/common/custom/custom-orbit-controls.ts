8fe9b05804e3d00618aaea5a3052f58f574ffa45
user1808 <tomasz.kozak04@gmail.com>
2025-01-18 19:15:21 +0100

    feat: check stores logic

--- src/rubik-cube-app/common/custom/custom-orbit-controls.ts
+++ src/rubik-cube-app/common/custom/custom-orbit-controls.ts
@@ -1,6 +1,6 @@
 import type { PerspectiveCamera } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { useOrbitControlsDataStore } from '@/stores/useOrbitControlsDataStore';
+import { useOrbitControlsDataStore } from '@/stores/use-orbit-controls-data-store';
 
 export class CustomOrbitControls extends OrbitControls {
   override readonly enablePan: boolean = false;
