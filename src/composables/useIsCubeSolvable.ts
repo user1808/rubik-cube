5b5baeb065779308a353759764eacbc1d0d062bd
user1808 <tomasz.kozak04@gmail.com>
2026-01-05 21:45:12 +0100

    feat: enhance cube solvability check with logical face values

--- src/composables/useIsCubeSolvable.ts
+++ src/composables/useIsCubeSolvable.ts
@@ -1,4 +1,6 @@
 import type { TCubeCommonNames } from '@/rubik-cube-app/rubik-cube/types/cube-common-name';
+import type { TCubeFaceColor } from '@/rubik-cube-app/rubik-cube/types/rubik-cube';
+import { useFacesLogicalValuesStore } from '@/stores/use-faces-logical-values-store';
 import { useSelectedCubeStore } from '@/stores/use-selected-cube-store';
 import { storeToRefs } from 'pinia';
 import { computed } from 'vue';
@@ -6,8 +8,13 @@ import { computed } from 'vue';
 export const useIsCubeSolvable = () => {
   const selectedCubeStore = useSelectedCubeStore();
   const { getCurrentCubeProperties } = storeToRefs(selectedCubeStore);
+  const facesLogicalValuesStore = useFacesLogicalValuesStore();
+  const { getFacesLogicalValues } = storeToRefs(facesLogicalValuesStore);
 
-  const isCubeSolvableAlghorithms: Record<TCubeCommonNames, () => boolean> = {
+  const isCubeSolvableAlghorithms: Record<
+    TCubeCommonNames,
+    (logicalValues: Record<string, Array<Nullable<TCubeFaceColor>>>) => boolean
+  > = {
     '2x2x2 Cube': () => true,
     '3x3x3 Cube': () => true,
     '4x4x4 Cube': () => true,
@@ -20,7 +27,12 @@ export const useIsCubeSolvable = () => {
     const cubeName = getCurrentCubeProperties.value?.commonName;
     if (!cubeName) return false;
 
-    return isCubeSolvableAlghorithms[cubeName]();
+    const facesLogicalValues = getFacesLogicalValues.value[cubeName];
+    if (!facesLogicalValues) return false;
+
+    console.log(facesLogicalValues);
+
+    return isCubeSolvableAlghorithms[cubeName](facesLogicalValues);
   });
 
   return {
