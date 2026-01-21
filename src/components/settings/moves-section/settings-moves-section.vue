e0ff0a6dbd65a84e4a8759a7d93ade919371c619
user1808 <tomasz.kozak04@gmail.com>
2026-01-21 20:55:21 +0100

    refactor: simplify moves section layout in settings component

--- src/components/settings/moves-section/settings-moves-section.vue
+++ src/components/settings/moves-section/settings-moves-section.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="relative flex size-full flex-col px-4 pb-4">
-    <h1 class="shrink-0 select-none text-nowrap py-4 text-xl tracking-tight text-white">
+  <div class="relative flex max-h-full w-full flex-col px-4 pb-4">
+    <h1 class="select-none text-nowrap py-4 text-xl tracking-tight text-white">
       Moves History For {{ currentCubeName ?? 'No cube selected' }}
     </h1>
-    <div class="flex min-h-0 flex-1">
-      <SettingsMovesHistory />
-    </div>
+    <SettingsMovesHistory />
   </div>
 </template>
 
